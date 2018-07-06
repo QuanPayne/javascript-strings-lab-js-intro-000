ccfb0c6ca49e718e32632d7cf2e05d494a05b167
Quan Payne <quanxavierp@gmail.com>
2018-07-06 01:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Quan Payne" + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
